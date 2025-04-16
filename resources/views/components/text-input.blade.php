6897b46ae966b8718e01ab3618721c30196ad456
Ondřej Mlčák <ondrejmlcak14@email.cz>
2025-04-16 12:44:33 +0200

    DFEFFE

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,12 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-black dark:border-black dark:bg-black dark:text-white focus:border-white dark:focus:border-white focus:ring-white dark:focus:ring-white rounded-md shadow-sm']) }}>
+<input @disabled($disabled)
+    {{ $attributes->merge([
+        'class' =>
+        'border-black dark:border-black dark:bg-black dark:text-white
+        focus:border-white dark:focus:border-white
+        focus:ring-white dark:focus:ring-white
+        rounded-md shadow-sm
+        px-4 py-3 text-base w-full max-w-xs
+        transition duration-200 ease-in-out'
+    ]) }}>
